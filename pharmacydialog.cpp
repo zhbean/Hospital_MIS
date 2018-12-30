01dd1bdbee20cdb0ea65cfd472d233b74242f960
WWB <s778877s@qq.com>
2018-12-29 20:37:25 -0800

    WWB 加了显示栏里面的员工号等信息

--- pharmacydialog.cpp
+++ pharmacydialog.cpp
@@ -109,6 +109,17 @@ void PharmacyDialog::on_pushButton_selectDrug_clicked()
 void PharmacyDialog::getAccountAndToolTip(int account,int dpmdetail)//account为账号即员工号，dpmdetail为科室详情号
 {
     staff_id=account;
+    dbManager db;
+    db.openDB();
+    QStringList information=db.getStaffAndRoom(account,dpmdetail);
+    if(!information.isEmpty()){
+        ui->StaffIdLabel->setNum(account);
+        ui->StaffNameLabel->setText(information.at(0));
+        ui->RoomNameLabel->setText(information.at(1));
+    }
+    else{
+        QMessageBox::information(NULL,"错误","数据查询错误！",QMessageBox::Yes|QMessageBox::No,QMessageBox::Yes);
+    }
 
 }
 
