c6f93cfc301fffb8205c5a719bd6a65737b9a666
WWB <s778877s@qq.com>
2019-01-01 05:14:09 -0800

    完成诊断功能，以及病人登记

--- registerwindow.cpp
+++ registerwindow.cpp
@@ -26,3 +26,31 @@ void registerWindow::getAccountAndToolTip(int account,int dpmdetail)//account为
         QMessageBox::information(NULL,"错误","数据查询错误！",QMessageBox::Yes|QMessageBox::No,QMessageBox::Yes);
     }
 }
+
+void registerWindow::on_registerPushButton_clicked()
+{
+    QString sql="INSERT INTO patient(patient_name,ID,patient_age,patient_sex,patient_contact,emergency_contact,allergy_history)VALUES(?,?,?,?,?,?,?);";
+    dbManager db;
+    db.openDB();
+    QSqlQuery query;
+    query.prepare(sql);
+    query.bindValue(0,ui->NameLineEdit->text());
+    query.bindValue(1,ui->IDLineEdit->text());
+    query.bindValue(2,ui->AgeLineEdit->text());
+    if(ui->SexLineEdit->text()=="男"){
+        query.bindValue(3,1);
+    }else{
+        query.bindValue(3,0);
+    }
+    query.bindValue(4,ui->patientContactLineEdit->text());
+    query.bindValue(5,ui->emergencyContactLineEdit->text());
+    query.bindValue(6,ui->plainTextEdit->toPlainText());
+    bool x=query.exec();
+    if(x){
+        QMessageBox::information(NULL,"正确","插入成功",QMessageBox::Yes|QMessageBox::No,QMessageBox::Yes);
+
+    }else{
+        qDebug()<<query.lastError();
+
+    }
+}
